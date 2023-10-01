60862d985f8bfffac073c0591d5a2764f9bec485
shivangirajput123 <rajputshivangiii987@gmail.com>
2023-10-01 19:44:49 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import BookStoreCalculator from './Component/BookStoreCalculator';
+
+function App() {
+  return (
+   <div><BookStoreCalculator /></div>
+  );
+}
+
+export default App;
---
+++ src/Component/BookStoreCalculator.js
@@ -0,0 +1,78 @@
+import React, { Component } from "react";
+
+
+class BookStoreCalculator extends Component {
+    constructor() {
+        super();
+        this.state = {
+            basket: [],
+            totalPrice: null,
+        };
+    }
+
+    handleAddToBasket = book => {
+        this.setState(prevState => ({
+            basket: [...prevState.basket, book],
+        }));
+    };
+
+    handleCalculatePrice = () => {
+        fetch('http://localhost/Calculate_book_pricing/api/Bookstore/calculatePrice', {
+            method: 'POST', 
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ basket: this.state.basket }),
+        })
+        .then(response => response.json())
+        .then(data => {
+            this.setState({ totalPrice: data.result.data }); // Corrected the state variable name
+        })
+        .catch(error => {
+            console.error('Error calculating price:', error);
+        });
+    };
+    
+
+    render() {
+        return (
+            <div>
+                <h2>Bookstore Calculator</h2>
+            <div>
+            <h3>Add Books to Basket</h3>
+            <button onClick={() =>
+            this.handleAddToBasket('book1')}> Add Book 1</button>
+           <button onClick={() =>
+            this.handleAddToBasket('book2')}> Add Book 2</button>
+            <button onClick={() =>
+            this.handleAddToBasket('book3')}> Add Book 3</button>
+            <button onClick={() =>
+            this.handleAddToBasket('book4')}> Add Book 4</button>
+            <button onClick={() =>
+            this.handleAddToBasket('book5')}> Add Book 5</button>
+            </div>
+            <div>
+                <h3>Shopping Basket</h3>
+                <ul> 
+                {this.state.basket.map((book, index) =>
+                (
+                    <li key={index}>{book}</li>
+                ))}</ul>
+            </div>
+            <div>
+                <button onClick={this.handleCalculatePrice}>Calculate Price</button>
+            </div>
+
+            <div>
+                <h3>Total Price</h3>
+                <p>$
+{this.state.totalPrice !== null ? this.state.totalPrice.toFixed(2) : 'Calculating...'}
+</p>
+
+            </div>
+             
+        </div>
+        );
+    }
+}
+export default BookStoreCalculator;
